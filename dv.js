4fe0e02155feab61c8963b11ea939352e0af5cdc
kisal-dv <kisalangira119@gmail.com>
2025-03-17 21:13:19 +0530

    callback

--- dv.js
+++ dv.js
@@ -1,28 +1,44 @@
-let book = {
-    title:"dragon",
-    author:"kisal",
-    age:23,
-    genres: ["Fiction", "Classic", "Drama"],
-    isavailabale:true,
+class Animal {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    speak() {
+      console.log(`${this.name} makes a sound.`);
+    }
+  }
+  
+  const dog = new Animal("Dog");
+  dog.speak();     
+  
 
-};
+// little call back example
 
-function printbookdetails(book) {
-
-    console.log(`Title: ${book.title}`);
-    console.log(`Author: ${book.author}`);
-    console.log(`Age: ${book.age}`);
-    console.log(`Genres: ${book.genres.join(',')}`);
+function greet(name, callback) {
+    console.log("hell0," + name);
+    callback();
     
 }
 
-if (book.isavailabale) {
-    console.log("Book is available .")
-    
-} else {
-console.log("out of stock !")
+function saygoodbye() {
+    console.log("good bye !")
     
 }
+greet("kisal",saygoodbye);
 
 
-printbookdetails(book);
+///
+
+function greeting1(name, callback) {
+    console.log("Hello," +name)
+    callback();
+    
+}
+function saygoodmorining() {
+    console.log("Good morining  !");
+    
+}
+greeting1("kisal 1 ",saygoodmorining);
+
+
+////////////////////////
